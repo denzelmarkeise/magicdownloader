364b78834ab09baaeb16bb054a83a73f291f1aea
denzelmarkeise <137828085+denzelmarkeise@users.noreply.github.com>
2023-11-12 15:15:06 -0600

    updated to pull folders and files

--- magicdownloader.py
+++ magicdownloader.py
@@ -1,43 +1,40 @@
 import os
 import shutil
 
-def organize_files(source_folder, destination_folder):
+def organize_files_and_folders(source_folder, destination_folder):
     # ---CREATE THE DESTINATION FOLDER IF IT DOES NOT EXIST!!---
-    
     if not os.path.exists(destination_folder):
         os.makedirs(destination_folder)
 
-    # ---ITERATE THROUGH ALL FILES IN THE SOURCE FOLDER!!---
-    
-    for filename in os.listdir(source_folder):
-        source_path = os.path.join(source_folder, filename)
+    # ---ITERATE THROUGH ALL ITEMS (FILES AND FOLDERS) IN THE SOURCE FOLDER!!---
+    for item in os.listdir(source_folder):
+        source_path = os.path.join(source_folder, item)
 
-        # ---CHECK IF IT'S A FILE!!---
-
+        # ---CHECK IF IT'S A FILE OR A FOLDER!!---
         if os.path.isfile(source_path):
-            # ---GET THE FILE EXTENSION!!----
-            
-            _, extension = os.path.splitext(filename)
-
-            # ---CREATE A SUBDIRECTORY BASED ON THE FILE EXTENSION!!---
-            
+            # ---FOR FILES: GET THE FILE EXTENSION!!---
+            _, extension = os.path.splitext(item)
             destination_path = os.path.join(destination_folder, extension[1:])
-            if not os.path.exists(destination_path):
-                os.makedirs(destination_path)
+        elif os.path.isdir(source_path):
+            # ---FOR FOLDERS: USE THE FOLDER NAME!!---
+            destination_path = os.path.join(destination_folder, item)
+        else:
+            # Skip if it's neither a file nor a folder
+            continue
 
-            # ---MOVE THE FILE TO THE APPROPRIATE SUBDIRECTORY!!---
-            
-            shutil.move(source_path, os.path.join(destination_path, filename))
+        # ---CREATE THE SUBDIRECTORY BASED ON THE ITEM TYPE (FILE OR FOLDER)!---
+        if not os.path.exists(destination_path):
+            os.makedirs(destination_path)
+
+        # ---MOVE THE ITEM TO THE APPROPRIATE SUBDIRECTORY!---
+        shutil.move(source_path, os.path.join(destination_path, item))
 
 if __name__ == "__main__":
-    
-    # ---SPECIFY THE SOURCE & DESTINATION FOLDERS!!---
-    
+    # ---SPECIFY THE SOURCE & DESTINATION FOLDERS!---
     source_folder = r"C:\Users\yourusername\Downloads"
-    destination_folder = r"C:\Users\yourusername\Organized Downloads"
+    destination_folder = r"/Users/yourusername/Documents/OrganizedDownloads"
 
-    # ---ORGANIZE FILES!!---
+    # ---ORGANIZE FILES AND FOLDERS!---
+    organize_files_and_folders(source_folder, destination_folder)
 
-    organize_files(source_folder, destination_folder)
-
-    print("File organization has been completed!")
+    print("File and folder organization has been completed!")
